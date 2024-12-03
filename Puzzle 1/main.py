6bfbc5fe307f3e39a08a53b78368e2e35732dc83
Parikshit Sah <parikshit.sah12@gmail.com>
2024-12-02 23:13:23 -0500

    ADD: Solved with built in sort function

--- Puzzle 1/main.py
+++ Puzzle 1/main.py
@@ -6,4 +6,12 @@ def quick_sort(list, length):
 # Load the data from the file
 left, right = np.loadtxt('Puzzle 1\puzzle input.txt', unpack=True, dtype=int)
 
+left.sort()
+right.sort()
 
+sum = 0
+for i, _ in enumerate(left):
+    sum = sum + abs(left[i]- right[i])
+
+
+print(sum)
